8257d8f138cd941059a68424794d41cd8692f7c6
Sydney Kanuga <skanuga1@jhu.edu>
2024-09-16 19:04:11 -0400

    Titles and Legend

--- Trigonometry.py
+++ Trigonometry.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,19 +7,22 @@ import numpy as np
 def plot(function):
     x = np.linspace(-10, 10, 40)
     for i in function:
-        if "sin" in function:
+        if "sin" in function and "sinc" not in function:
             plt.plot (x, np.sin(x), "blue", label = "sin(x)")
         if "cos" in function:
             plt.plot (x, np.cos(x), "magenta", label = "cos(x)")    
-        if "sinc" in function:
+        if "sinc" in function and "sin" not in function:
             plt.plot (x, np.sinc(x), "teal", label = "sinc(x)")
-        plt.plot()
+        if "sin" in function and "sinc" in function:
+            plt.plot (x, np.sin(x), "blue", label = "sin(x)")
+            plt.plot (x, np.sinc(x), "teal", label = "sinc(x)")
+        plt.title ("Trigonometry Plot")
+        plt.legend()
+        plt.xlaber("x")
+        plt.ylabel(function)
         plt.show()
         
-        
-        
-    
-    
+   
 function = input('--function = ')
 plot(function)
 
