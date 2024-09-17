2f12c17a35dc18c3169ab2b10677e02dd2d1160c
Sydney Kanuga <skanuga1@jhu.edu>
2024-09-16 21:34:27 -0400

    first attempt at Q4

--- Trigonometry.py
+++ Trigonometry.py
@@ -4,10 +4,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-#Question 1
+
 def plot(function):
-    x = np.linspace(-10, 10, 40)
-    for i in function:
+    """
+    
+
+    Parameters
+    ----------
+    function : function, or list of functions to be graphed 
+
+    Returns plot of all functions graphed with legend and axes titled
+    -------
+    None.
+
+    """
+    x = np.linspace(-10, 10, 400) #create a list of x values
+    for i in function: #plot functions as thy appear in the input list of functions
         if "sin" in function and "sinc" not in function:
             plt.plot (x, np.sin(x), "blue", label = "sin(x)")
         if "cos" in function:
@@ -17,19 +29,33 @@ def plot(function):
         if "sin" in function and "sinc" in function:
             plt.plot (x, np.sin(x), "blue", label = "sin(x)")
             plt.plot (x, np.sinc(x), "teal", label = "sinc(x)")
-        plt.title ("Trigonometry Plot")
+            #plot title, legend, axis titles
+        plt.title ("Trigonometry Plot") 
         plt.legend(loc = "upper left")
         plt.xlabel("x")
         plt.ylabel(function)
         plt.show()
         return      
+   
 
-#Question 2
 def table(function, path):
-    x = np.arange(-10,10.05,0.05)
-    x = np.round(x, 3)
-   # x = np.linspace(-10,10,400)
-    df = pd.DataFrame(index = x)
+    """
+    
+
+    Parameters
+    ----------
+    function : list of functions to be calculated
+    path : path of file generated
+
+    Returns txt file with a table of function values
+    -------
+    None.
+
+    """
+    x = np.arange(-10,10.05,0.05)  #create a list of x values - becomes left most column of txt file
+    x = np.round(x, 3) #round x values to 2 decimal places
+  
+    df = pd.DataFrame(index = x) #use data frame to generate a table
     for i in function:
         if "sin" in function and "sinc" not in function:
             df['sin(x)'] = np.sin(x)
@@ -40,20 +66,37 @@ def table(function, path):
         if "sin" in function and "sinc" in function:
             df['sinc(x)'] = np.sinc(x)
             df['sin(x)'] = np.sin(x)
+            # enter data points for each of the requested functions into the data frame
     string = df.to_string(header=True, index=True)
-    f = open (path, 'w')
+    f = open(path, 'w') #open and write into the txt file
     f.write(string)
     f.close()
     
+# def readfromfile(readfile):
+#     ##stopped here
+    
+#     return
 
 
 
+def print(img, filename):
+    
+    if 'jpeg' in img:
+        plt.savefig(path, format='jpg')
+    if 'eps' in img:
+        plt.savefig(path, format='eps')
+    if 'pdf' in img:
+        plt.savefig(path, format='pdf')
+    ##UNTESTED
+    
+    
 
-
+#readfile = input('--read from file = ')
 filename = input ('--write = ')
 path = rf"/Users/skanuga/Desktop/{filename}"
 function = input('--function = ')
+plot = plot(function)
+img = input('--print = ')
 plot(function)
 table = table(function, path)
 print(table)
-
