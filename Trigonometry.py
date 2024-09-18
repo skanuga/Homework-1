b3a788becf7ac6a3a1f5f6a43dbe573084afddfc
Sydney Kanuga <skanuga1@jhu.edu>
2024-09-18 12:57:18 -0400

    Bruteforcing of Q1 and 2

--- Trigonometry.py
+++ Trigonometry.py
@@ -20,23 +20,40 @@ def plot(function):
     """
     x = np.linspace(-10, 10, 400) #create a list of x values
     for i in function: #plot functions as thy appear in the input list of functions
-        if "sin" in function and "sinc" not in function:
+    
+        if "sin" in function and "sinc" not in function and len(function) <= 5: #sin alone
             plt.plot (x, np.sin(x), "blue", label = "sin(x)")
-        if "cos" in function:
-            plt.plot (x, np.cos(x), "magenta", label = "cos(x)")    
-        if "sinc" in function and "sin" not in function:
+            # print('sin')
+        if "cos" in function and len(function) <= 5: #cos alone
+            plt.plot (x, np.cos(x), "magenta", label = "cos(x)")  
+           # print('cos')
+        if "sinc" in function and len(function)==4: #sinc alone
             plt.plot (x, np.sinc(x), "teal", label = "sinc(x)")
-        if "sin" in function and "sinc" in function:
+            #print('sinc')
+        if "cos" in function and "sinc" in function and len(function) >= 8: #cos,sinc
+             plt.plot (x, np.cos(x), "magenta", label = "cos(x)")  
+             plt.plot (x, np.sinc(x), "teal", label = "sinc(x)")
+             #print('cos,sin')
+        if "sin" in function and "sinc" in function and "cos" not in function and len(function) >= 8: #sin,sinc
+             plt.plot (x, np.sin(x), "blue", label = "sin(x)")
+             plt.plot (x, np.sinc(x), "teal", label = "sinc(x)")
+             #print('sin,sinc')
+        if "cos" in function and "sin" in function and len(function) <= 7: #cos,sinc
+             plt.plot (x, np.cos(x), "magenta", label = "cos(x)")  
+             plt.plot (x, np.sin(x), "blue", label = "sin(x)")
+             #print('cos,sin')        
+        if len(function) > 10: #sin,sinc,cos
             plt.plot (x, np.sin(x), "blue", label = "sin(x)")
-            plt.plot (x, np.sinc(x), "teal", label = "sinc(x)")
-            #plot title, legend, axis titles
+           # print('all')
+            
+     #plot title, legend, axis titles
         plt.title ("Trigonometry Plot") 
         plt.legend(loc = "upper left")
         plt.xlabel("x")
         plt.ylabel(function)
         plt.show()
         return      
-   
+
 
 def table(function, path):
     """
@@ -57,15 +74,25 @@ def table(function, path):
   
     df = pd.DataFrame(index = x) #use data frame to generate a table
     for i in function:
-        if "sin" in function and "sinc" not in function:
+        if "sin" in function and "sinc" not in function and len(function) <= 5: #sin alone
             df['sin(x)'] = np.sin(x)
-        if "cos" in function:
+        if "cos" in function and len(function) <= 5: #cos alone
             df['cos(x)'] = np.cos(x)
-        if "sinc" in function and "sin" not in function:
+        if "sinc" in function and len(function)==4: #sinc alone
             df['sinc(x)'] = np.sinc(x)
-        if "sin" in function and "sinc" in function:
+        if "cos" in function and "sinc" in function and len(function) >= 8: #cos,sinc
+            df['cos(x)'] = np.cos(x)
+            df['sinc(x)'] = np.sinc(x)
+        if "sin" in function and "sinc" in function and "cos" not in function and len(function) >= 8: #sin,sinc
             df['sinc(x)'] = np.sinc(x)
             df['sin(x)'] = np.sin(x)
+        if "cos" in function and "sin" in function and len(function) <= 7: #cos,sin
+            df['cos(x)'] = np.cos(x)
+            df['sin(x)'] = np.sin(x)
+        if len(function) > 10: #sin,sinc,cos
+            df['sinc(x)'] = np.sinc(x)
+            df['sin(x)'] = np.sin(x)
+            df['cos(x)'] = np.cos(x)
             # enter data points for each of the requested functions into the data frame
     string = df.to_string(header=True, index=True)
     f = open(path, 'w') #open and write into the txt file
